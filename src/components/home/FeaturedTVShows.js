9caf7131852ddc78d965ccb15973786576d9b383
sahilseneca2022 <106559325+sahilseneca2022@users.noreply.github.com>
2022-07-16 02:05:27 +0530

    Navigation fix

--- src/components/home/FeaturedTVShows.js
+++ src/components/home/FeaturedTVShows.js
@@ -1,62 +1,39 @@
 import React from "react";
 import Slider from "react-slick";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-class FeaturedTVShows extends React.Component {
-  // Constructor
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      items: [],
-      DataisLoaded: false,
-    };
-  }
-
-  componentDidMount() {
+const FeaturedTVShows = () => {
+  const [items, setItems] = useState();
+  useEffect(() => {
     fetch("https://moview-backend.herokuapp.com/featuredTvShowsData")
       .then((res) => res.json())
       .then((json) => {
-        console.log(json);
-        this.setState({
-          items: json,
-          DataisLoaded: true,
-        });
+        setItems(json);
       });
-  }
-  render() {
-    const { DataisLoaded, items } = this.state;
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 6,
-      slidesToScroll: 1,
-    };
-    if (!DataisLoaded)
-      return (
-        <div>
-          <h1> Pleses wait some time.... </h1>
-        </div>
-      );
-    return (
-      <div className="p-6">
-        <h2 className="font-bold text-lg">Featured Movies</h2>
-        <Slider className="py-6" {...settings}>
-          {items?.length > 0 &&
-            items.map((item, index) => (
-              <div
-                key={index}
-                onClick={() => navigation.navigate("details/tv/" + item.id)}
-              >
-                <h3>{item.title}</h3>
-                <img src={item.image} width={200} height={400} />
-              </div>
-            ))}
-        </Slider>
-      </div>
-    );
-  }
-}
+  }, []);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+  };
+  let nav = useNavigate();
+  return (
+    <div className="p-6">
+      <h2 className="font-bold text-lg">Featured Movies</h2>
+      <Slider className="py-6" {...settings}>
+        {items?.length > 0 &&
+          items.map((item, index) => (
+            <div key={index} onClick={() => nav("details/movie/" + item.id)}>
+              <h3>{item.title}</h3>
+              <img src={item.image} width={200} height={400} />
+            </div>
+          ))}
+      </Slider>
+    </div>
+  );
+};
 
 export default FeaturedTVShows;
--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,58 +1,31 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-class Movies extends React.Component {
-  // Constructor
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      items: [],
-      DataisLoaded: false,
-    };
-  }
-
-  componentDidMount() {
+const Movies = () => {
+  const [items, setItems] = useState();
+  useEffect(() => {
     fetch("https://moview-backend.herokuapp.com/movies")
       .then((res) => res.json())
       .then((json) => {
-        console.log(json);
-        this.setState({
-          items: json,
-          DataisLoaded: true,
-        });
+        setItems(json);
       });
-  }
-  render() {
-    const { DataisLoaded, items } = this.state;
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 6,
-      slidesToScroll: 1,
-    };
-    if (!DataisLoaded)
-      return (
-        <div>
-          <h1> Please wait some time.... </h1>{" "}
-        </div>
-      );
-    return (
-      <div className="grid grid-cols-8 gap-4">
-        {items?.length > 0 &&
-          items.map((item, index) => (
-            <div
-              key={index}
-              onClick={() => navigation.navigate("/Details/movie/" + item.id)}
-            >
-              <h3>{item.title}</h3>
-              <img src={item.image} />
-            </div>
-          ))}
-      </div>
-    );
-  }
-}
+  }, []);
+  let nav = useNavigate();
+  return (
+    <div className="grid grid-cols-8 gap-4">
+      {items?.length > 0 &&
+        items.map((item, index) => (
+          <div
+            key={index}
+            onClick={() => navigation.navigate("/Details/movie/" + item.id)}
+          >
+            <h3>{item.title}</h3>
+            <img src={item.image} />
+          </div>
+        ))}
+    </div>
+  );
+};
 
 export default Movies;
