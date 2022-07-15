a7755ed32e1c06d240b6c5254e470c7afb6749f7
sahilseneca2022 <106559325+sahilseneca2022@users.noreply.github.com>
2022-07-16 02:21:49 +0530

    fix tv

--- src/pages/TvShows.js
+++ src/pages/TvShows.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 const TvShows = () => {
   const [items, setItems] = useState();
   useEffect(() => {
-    fetch("https://moview-backend.herokuapp.com/movies")
+    fetch("https://moview-backend.herokuapp.com/Tvshows")
       .then((res) => res.json())
       .then((json) => {
         setItems(json);
@@ -16,7 +16,7 @@ const TvShows = () => {
     <div className="grid grid-cols-8 gap-4">
       {items?.length > 0 &&
         items.map((item, index) => (
-          <div key={index} onClick={() => nav("/Details/movie/" + item.id)}>
+          <div key={index} onClick={() => nav("/Details/tv/" + item.id)}>
             <h3>{item.title}</h3>
             <img src={item.image} />
           </div>
